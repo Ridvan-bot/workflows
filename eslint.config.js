baf56fdef4f8f1bbadfa2d64e10c08f48631af29
Robin Pohlman <robin.pohlman@gmail.com>
2024-11-12 11:05:45 +0100

    test: migrate from .json to .js for eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
 module.exports = {
+    extends: 'standard',
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'double']
+    },
     overrides: [
       {
-        files: ['*.js', '*.ts', '*.tsx'],
-      },
-    ],
+        files: ['*.ts', '*.tsx'],
+        rules: {
+        }
+      }
+    ]
   };
   
