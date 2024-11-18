68013518d80cbc807e3f78dd072725a3a9edeea5
Robin Pohlman <robin.pohlman@gmail.com>
2024-11-18 11:47:18 +0100

    chore: install jest react libs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom', // jsdom is required for React tests
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest', // Tells Jest to use ts-jest for TypeScript files
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   };
   
