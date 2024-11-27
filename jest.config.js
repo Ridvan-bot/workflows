6b01afe9d4f4aad65978b7cf47d599e64f033c5f
Robin Pohlman <robin.pohlman@gmail.com>
2024-11-27 07:28:36 +0100

    fix: test folder location

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom', // jsdom is required for React tests
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest', // Tells Jest to use ts-jest for TypeScript files
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom', // jsdom is required for React tests
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Tells Jest to use ts-jest for TypeScript files
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
